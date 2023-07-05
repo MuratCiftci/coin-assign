0479516d2e472d5c5e32d0ed215f3da019014113
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-07-05 16:02:50 +0300

    feat: add axios config

---
+++ configs/axiosConfig.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: "https://api.coingecko.com/api/v3"
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    debugger
+    if (response.status === 500 || response.status === 404) {
+      return Promise.reject(response);
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401 || error.response?.status === 500) {
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
