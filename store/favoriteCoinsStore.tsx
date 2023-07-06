92ee7ed0231de3982917bca6f5de0a762f69d920
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-07-06 10:07:10 +0300

    feat: add persist to zustand global store

--- store/favoriteCoinsStore.tsx
+++ store/favoriteCoinsStore.tsx
@@ -1,17 +1,25 @@
 import { create } from "zustand";
-
+import { persist, createJSONStorage } from "zustand/middleware";
 interface FavoriteCoinsStore {
   favoriteCoins: string[];
   addFavoriteCoin: (coin: string) => void;
   removeFavoriteCoin: (coin: string) => void;
 }
 
-export const useFavoriteCoinsStore = create<FavoriteCoinsStore>((set) => ({
-  favoriteCoins: [],
-  addFavoriteCoin: (coin) =>
-    set((state) => ({ favoriteCoins: [...state.favoriteCoins, coin] })),
-  removeFavoriteCoin: (coin) =>
-    set((state) => ({
-      favoriteCoins: state.favoriteCoins.filter((c) => c !== coin),
-    })),
-}));
+export const useFavoriteCoinsStore = create<FavoriteCoinsStore>()(
+  persist(
+    (set) => ({
+      favoriteCoins: [],
+      addFavoriteCoin: (coin) =>
+        set((state) => ({ favoriteCoins: [...state.favoriteCoins, coin] })),
+      removeFavoriteCoin: (coin) =>
+        set((state) => ({
+          favoriteCoins: state.favoriteCoins.filter((c) => c !== coin),
+        })),
+    }),
+    {
+      name: "favoriteCoins",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
