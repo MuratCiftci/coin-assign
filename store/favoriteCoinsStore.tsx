d80a1b380128d94f7adbc46b690bdf4e8f64994c
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-07-06 10:18:48 +0300

    refactor: remove persist from favorite store

--- store/favoriteCoinsStore.tsx
+++ store/favoriteCoinsStore.tsx
@@ -1,25 +1,17 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+
 interface FavoriteCoinsStore {
   favoriteCoins: string[];
   addFavoriteCoin: (coin: string) => void;
   removeFavoriteCoin: (coin: string) => void;
 }
 
-export const useFavoriteCoinsStore = create<FavoriteCoinsStore>()(
-  persist(
-    (set) => ({
-      favoriteCoins: [],
-      addFavoriteCoin: (coin) =>
-        set((state) => ({ favoriteCoins: [...state.favoriteCoins, coin] })),
-      removeFavoriteCoin: (coin) =>
-        set((state) => ({
-          favoriteCoins: state.favoriteCoins.filter((c) => c !== coin),
-        })),
-    }),
-    {
-      name: "favoriteCoins",
-      storage: createJSONStorage(() => localStorage),
-    }
-  )
-);
+export const useFavoriteCoinsStore = create<FavoriteCoinsStore>((set) => ({
+  favoriteCoins: [],
+  addFavoriteCoin: (coin) =>
+    set((state) => ({ favoriteCoins: [...state.favoriteCoins, coin] })),
+  removeFavoriteCoin: (coin) =>
+    set((state) => ({
+      favoriteCoins: state.favoriteCoins.filter((c) => c !== coin),
+    })),
+}));
