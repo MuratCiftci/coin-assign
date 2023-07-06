61a2699571c3b0e38f798b1ff776024e397d78fe
MuratCiftci <mciftci174@posta.pau.edu.tr>
2023-07-06 09:54:43 +0300

    add prisma generate in build

--- hooks/useParams.ts
+++ hooks/useParams.ts
@@ -2,8 +2,6 @@ import { useRouter } from "next/router";
 
 export const useParams = () => {
     const router = useRouter();
-    console.log(router);
     const { id } = router.query;
-    console.log(id);
     return id;
 }
